8c01fd457eff2768bda0fb2b69919607c928b346
Jamie956 <752492509@qq.com>
2021-03-29 15:33:10 +0800

    up

--- basic/src/main/java/com/jamie/BasicMain.java
+++ basic/src/main/java/com/jamie/BasicMain.java
@@ -494,6 +494,34 @@ public class BasicMain {
             return false;
         }
     }
+
+    /**
+     * 取模
+     */
+    @Test
+    public void asa(){
+//        int a = 499 % 500;//499
+//        int b = 500 % 500;//0
+//        int c = 501 % 500;//1
+//        int d = 999 % 500;//499
+//        int e = 1000 % 500;//0
+//        int f = 1001 % 500;//1
+
+//        int g = 499 / 500;//0
+//        int h = 500 / 500;//1
+//        int i = 501 / 500;//1
+//        int j = 999 / 500;//1
+//        int k = 1000 / 500;//2
+//        int l = 1001 / 500;//2
+
+        int a = Math.floorDiv(499, 500);//0
+        int b = Math.floorDiv(500, 500);//1
+        int c = Math.floorDiv(501, 500);//1
+        int d = Math.floorDiv(999, 500);//1
+        int e = Math.floorDiv(1000, 500);//2
+        int f = Math.floorDiv(1001, 500);//2
+    }
+
 }
 
 

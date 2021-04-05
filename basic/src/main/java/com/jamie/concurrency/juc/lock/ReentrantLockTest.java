5e943ce0da6f01fe48397406468c51a7fb2e3371
Jamie956 <752492509@qq.com>
2021-04-05 11:49:20 +0800

    up

--- basic/src/main/java/com/jamie/concurrency/juc/lock/ReentrantLockTest.java
+++ basic/src/main/java/com/jamie/concurrency/juc/lock/ReentrantLockTest.java
@@ -16,7 +16,16 @@ public class ReentrantLockTest {
         }
     }
 
-    public static void work(ReentrantLock lock) {
+    /**
+     * 没有锁时，任务无序执行
+     */
+    public static void noLockWorkTest() {
+        new Thread(() -> noLockWork()).start();
+        new Thread(() -> noLockWork()).start();
+        new Thread(() -> noLockWork()).start();
+    }
+
+    public static void lockWork(ReentrantLock lock) {
         lock.lock();
         try {
             for (int i = 0; i < 10; i++) {
@@ -31,90 +40,76 @@ public class ReentrantLockTest {
     }
 
     /**
-     * 没有锁时，任务无序执行
-     */
-    public static void test0() {
-        new Thread(() -> noLockWork()).start();
-        new Thread(() -> noLockWork()).start();
-        new Thread(() -> noLockWork()).start();
-    }
-
-    /**
      * 3个线程使用同一个 ReentrantLock，上锁的代码其他线程不可进入
      */
-    public static void test1() {
+    public static void lockWorkTest() {
         ReentrantLock lock = new ReentrantLock();
-        new Thread(() -> work(lock)).start();
-        new Thread(() -> work(lock)).start();
-        new Thread(() -> work(lock)).start();
+        new Thread(() -> lockWork(lock)).start();
+        new Thread(() -> lockWork(lock)).start();
+        new Thread(() -> lockWork(lock)).start();
     }
 
-    public static void reentrantWork(ReentrantLock lock) {
-        try {
-            lock.lock();//state==1
-            Thread.sleep(1000);
-            System.out.println(Thread.currentThread().getName() + "第一次嵌套内lock 的 state=" + lock.getHoldCount());
-            try {
-                lock.lock();//state==2
-                Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + "第二次嵌套内lock 的 state=" + lock.getHoldCount());
-                try {
-                    lock.lock();//state==3
-                    Thread.sleep(1000);
-                    System.out.println(Thread.currentThread().getName() + "第三次嵌套内lock 的 state=" + lock.getHoldCount());
-                } finally {
-                    lock.unlock();
-                }
-            } finally {
-                lock.unlock();
-            }
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }finally {
-            lock.unlock();
-        }
+    public static void reLockWork(ReentrantLock lock) {
 
         try {
             lock.lock();
             Thread.sleep(1000);
-            System.out.println(Thread.currentThread().getName() + "第四次嵌套外lock 的 state=" + lock.getHoldCount());
+            System.out.println(Thread.currentThread().getName() + "第一次嵌套内lock 的 state=" + lock.getHoldCount());
+
+            lock.lock();
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + "第二次嵌套内lock 的 state=" + lock.getHoldCount());
+
+            lock.lock();
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + "第三次嵌套内lock 的 state=" + lock.getHoldCount());
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             lock.unlock();
+            lock.unlock();
+            lock.unlock();
+            System.out.println("锁释放");
         }
+
     }
 
     /**
      * 可重入锁
      * 同一个线程能进入 同一把锁 每一个锁住的代码块，通过CAS 修改state，嵌套的lock 会累计计算，嵌套外的会重置
      */
-    public static void test2() {
+    public static void reLockWorkTest() {
         ReentrantLock lock = new ReentrantLock();
-        new Thread(() -> reentrantWork(lock)).start();
-        new Thread(() -> reentrantWork(lock)).start();
+        new Thread(() -> reLockWork(lock)).start();
+        new Thread(() -> reLockWork(lock)).start();
     }
 
     public static void twoLockWork(ReentrantLock a, ReentrantLock b) {
         try {
             a.lock();
-            Thread.sleep(1000);
-            System.out.println(Thread.currentThread().getName() + "第一次嵌套内lock 的 state=" + a.getHoldCount());
-            try {
-                b.lock();
+            for (int i = 0; i < 10; i++) {
                 Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + "第二次嵌套内lock 的 state=" + b.getHoldCount());
-            } finally {
-                b.unlock();
+                System.out.println(Thread.currentThread().getName() + " lock 1 i=" + i);
             }
-        }catch (InterruptedException e){
+
+            b.lock();
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " lock 2 i=" + i);
+            }
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             a.unlock();
+            b.unlock();
         }
     }
 
-    public static void twoLock() {
+    /**
+     * 必要要a锁 全部执行完
+     */
+    public static void twoLockTest() {
         ReentrantLock a = new ReentrantLock();
         ReentrantLock b = new ReentrantLock();
         new Thread(() -> twoLockWork(a, b)).start();
@@ -128,9 +123,9 @@ public class ReentrantLockTest {
                 Thread.sleep(100);
                 System.out.println(Thread.currentThread().getName() + " lock 1 i=" + i);
             }
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             a.unlock();
         }
 
@@ -150,7 +145,7 @@ public class ReentrantLockTest {
     /**
      * 第一条线程 跑完a lock 代码块，第二条线程开始跑a lock 代码块，第一条线程的 b lock 代码块 和 第二条线程的 a lock 交替执行
      */
-    public static void twoLock2() {
+    public static void twoLockTest2() {
         ReentrantLock a = new ReentrantLock();
         ReentrantLock b = new ReentrantLock();
         new Thread(() -> twoLockWork2(a, b)).start();
@@ -158,8 +153,6 @@ public class ReentrantLockTest {
     }
 
     public static void main(String[] args) {
-//        test2();
-//        twoLock();
-        twoLock2();
+        twoLockTest2();
     }
 }

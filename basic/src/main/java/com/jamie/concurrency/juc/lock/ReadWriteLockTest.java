6c473fb3871ea20c6b628746925766e30818d58b
Jamie956 <752492509@qq.com>
2021-04-05 21:11:38 +0800

    up

--- basic/src/main/java/com/jamie/concurrency/juc/lock/ReadWriteLockTest.java
+++ basic/src/main/java/com/jamie/concurrency/juc/lock/ReadWriteLockTest.java
@@ -1,40 +1,53 @@
 package com.jamie.concurrency.juc.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteLockTest {
+    /**
+     * 实例化一把读锁 和 写锁
+     */
+    static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
-    public static void task(Lock lock) {
-        lock.lock();
+    public static void task(Lock rwLock) {
+        //下一行打断点，选择 Thread，可以看到每条线程的断点
+        rwLock.lock();
         try {
             for (int i = 0; i < 10; i++) {
+                TimeUnit.SECONDS.sleep(1);
                 System.out.println(Thread.currentThread().getName() + " " + i);
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
-            lock.unlock();
+            rwLock.unlock();
         }
     }
 
-    //read lock test
-    public static void test1() {
-        Lock lock = new ReentrantReadWriteLock().readLock();
+    /**
+     * 锁共享，无序
+     */
+    public static void readLockTest() {
+        ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
 
-        new Thread(() -> task(lock)).start();
-        new Thread(() -> task(lock)).start();
-        new Thread(() -> task(lock)).start();
+        new Thread(() -> task(readLock)).start();
+        new Thread(() -> task(readLock)).start();
+        new Thread(() -> task(readLock)).start();
     }
 
-    //write lock test
-    public static void test2() {
-        Lock lock = new ReentrantReadWriteLock().writeLock();
+    /**
+     * 锁独占，有序
+     */
+    public static void writeLockTest() {
+        ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
 
-        new Thread(() -> task(lock)).start();
-        new Thread(() -> task(lock)).start();
-        new Thread(() -> task(lock)).start();
+        new Thread(() -> task(writeLock)).start();
+        new Thread(() -> task(writeLock)).start();
+        new Thread(() -> task(writeLock)).start();
     }
 
     public static void main(String[] args) {
-        test1();
+        readLockTest();
     }
 }

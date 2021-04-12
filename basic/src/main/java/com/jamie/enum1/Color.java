3f56f0bc3d6d2fa6659347de6702029da328be0b
Jamie956 <752492509@qq.com>
2021-04-12 22:53:23 +0800

    up

--- basic/src/main/java/com/jamie/enum1/Color.java
+++ basic/src/main/java/com/jamie/enum1/Color.java
@@ -1,40 +1,43 @@
 package com.jamie.enum1;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Getter
-@AllArgsConstructor
 public enum Color {
     /**
-     *
+     * Color(name, value)
      */
-    RED("red", "红色"), GREEN("green", "绿色"), BLANK("blank", "白色"), YELLO("yello", "黄色");
+    RED("1", "红色"), GREEN("2", "绿色"), BLANK("3", "白色"), YELLOW("4", "黄色");
 
-    // 成员变量
-    private final String name;
-    private final String value;
+    /**
+     * 成员变量
+     */
+    private final String code;
+    private final String chinese;
 
-    // 将数据缓存到map中
-    private static final Map<String, String> CACHE = new HashMap<>();
+    Color(String code, String chinese) {
+        this.code = code;
+        this.chinese = chinese;
+    }
 
-    static {
-        for (Color color : Color.values()) {
-            CACHE.put(color.getName(), color.getValue());
+    public String getCode() {
+        return code;
+    }
+
+    public String getChinese() {
+        return chinese;
+    }
+
+    public static void main(String[] args) {
+        for (Color value : Color.values()) {
+            System.out.println(value.name() + " " + value.getCode() + " " + value.getChinese());
         }
+
+        //根据枚举名字，获取指定的枚举实例
+        Color a = Color.valueOf("RED");
+        System.out.println(a);
+        Color c = Color.RED;
+        System.out.println(c);
+        //获取枚举的索引
+        int b = Color.valueOf("YELLOW").ordinal();
+        System.out.println(b);
     }
 
-    // 根据name查询value值
-    public static String getValueByName(String name) {
-        return CACHE.get(name);
-    }
-
-    @Test
-    public void enumtest3() {
-        String value = Color.getValueByName("red");
-    }
 }

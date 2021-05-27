9af9ecb08e54240073c9e098cc8704c372fe8f70
Jamie956 <752492509@qq.com>
2021-05-27 17:56:40 +0800

    up

---
+++ basic/src/main/java/com/jamie/Word2Pdf.java
@@ -0,0 +1,43 @@
+package com.jamie;
+
+import java.io.*;
+
+/**
+ * @Author: Zjm
+ * @Date: 2021/5/27 17:50
+ */
+public class Word2Pdf {
+
+    //需要安装 LibreOffice
+    private String sofficeDir = "D:\\software\\LibreOffice\\program";
+
+    public void word2pdf(String inPath, String outPath) throws Exception {
+        if (!new File(inPath).exists()) {
+            throw new FileNotFoundException();
+        }
+        String command = String.format("%s\\soffice --convert-to pdf:writer_pdf_Export %s --outdir %s", sofficeDir, inPath, outPath);
+        String output = this.executeCommand(command);
+        System.out.println("command=" + command);
+        System.out.println("output=" + output);
+    }
+
+    protected String executeCommand(String command) throws IOException, InterruptedException {
+        StringBuilder output = new StringBuilder();
+        Process p;
+        p = Runtime.getRuntime().exec(command);
+        p.waitFor();
+        try (InputStreamReader inputStreamReader = new InputStreamReader(p.getInputStream(), "UTF-8");
+             BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+        }
+        p.destroy();
+        return output.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Word2Pdf().word2pdf("D:\\aa.docx", "D:\\temp");
+    }
+}

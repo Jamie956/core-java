0df9b4c6ebf5b6ca392f031b04b92b82b1fa6c47
Jamie956 <752492509@qq.com>
2021-08-09 09:52:41 +0800

    up

---
+++ office/src/main/java/com/jamie/ReadExcel.java
@@ -0,0 +1,80 @@
+package com.jamie;
+
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.poi.hssf.usermodel.*;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+/**
+ * @author ZJM
+ * @date 2021/8/6 18:08
+ */
+public class ReadExcel {
+
+    /**
+     * 读取一个excel文件的内容
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        readTable2();
+    }
+
+
+    //通过对单元格遍历的形式来获取信息 ，这里要判断单元格的类型才可以取出值
+    public static void readTable() throws Exception {
+        InputStream ips = new FileInputStream("C:\\Users\\tgwzz\\Downloads\\heimao.xls");
+        HSSFWorkbook wb = new HSSFWorkbook(ips);
+        HSSFSheet sheet = wb.getSheetAt(0);
+        for (Iterator ite = sheet.rowIterator(); ite.hasNext(); ) {
+            HSSFRow row = (HSSFRow) ite.next();
+//            System.out.println();
+            row.getCell(0);
+            for (Iterator itet = row.cellIterator(); itet.hasNext(); ) {
+                HSSFCell cell = (HSSFCell) itet.next();
+                String cellData = cell.getRichStringCellValue().toString();
+                System.out.println(cellData);
+
+            }
+        }
+    }
+
+    /**
+     * 读取excel, 把每行的第一、二列写成一个一行json, 写到文件
+     * @throws Exception
+     */
+    public static void readTable2() throws Exception {
+        String inPath = "C:\\Users\\tgwzz\\Downloads\\heimao.xls";
+        String outPath = "C:\\Users\\tgwzz\\Downloads\\heimao00.txt";
+
+        try (InputStream ips = new FileInputStream(inPath); HSSFWorkbook wb = new HSSFWorkbook(ips); FileWriter fw = new FileWriter(outPath); BufferedWriter bw = new BufferedWriter(fw);) {
+
+            HSSFSheet sheet = wb.getSheetAt(0);
+            for (Iterator ite = sheet.rowIterator(); ite.hasNext(); ) {
+                HSSFRow row = (HSSFRow) ite.next();
+                if (row.getCell(0) != null && row.getCell(1) != null) {
+                    String id = row.getCell(0).getRichStringCellValue().toString();
+                    String content = row.getCell(1).getRichStringCellValue().toString();
+                    JSONObject line = new JSONObject();
+                    line.put("id", id);
+                    line.put("content", content);
+                    bw.write(line + "\r\n");
+                }else {
+                    System.out.println(1);
+                }
+            }
+            bw.flush();
+        }
+//        fw.close();
+//        bw.close();
+//        ips.close();
+    }
+
+}

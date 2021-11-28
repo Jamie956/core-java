9f2f0f918377cc30d275244661a96caefb6485a5
Jamie956 <752492509@qq.com>
2021-11-28 09:46:07 +0800

    rm

--- hive-udf/src/main/java/com/jamie/IDGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.jamie;
-
-public class IDGenerator {
-    private static Sequence sequence = new Sequence();
-    public static long nextId(){
-        return sequence.nextId();
-    }
-}

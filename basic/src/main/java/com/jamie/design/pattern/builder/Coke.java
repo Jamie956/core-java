88ba29a3a893ec8e2d2d3384c583cbb6378284ff
Jamie956 <752492509@qq.com>
2021-12-16 22:36:41 +0800

    设计模式-建造者模式

---
+++ basic/src/main/java/com/jamie/design/pattern/builder/Coke.java
@@ -0,0 +1,22 @@
+package com.jamie.design.pattern.builder;
+
+/**
+ * 冷饮的具体子类：可乐
+ */
+public class Coke extends ColdDrink {
+    /**
+     * 给可乐命名
+     */
+    @Override
+    public String name() {
+        return "Coke";
+    }
+
+    /**
+     * 给可乐定价
+     */
+    @Override
+    public float price() {
+        return 30.0f;
+    }
+}
---
+++ basic/src/main/java/com/jamie/design/pattern/builder/Pepsi.java
@@ -0,0 +1,22 @@
+package com.jamie.design.pattern.builder;
+
+/**
+ * 冷饮的具体实现子类
+ */
+public class Pepsi extends ColdDrink {
+    /**
+     * 给百事命名
+     */
+    @Override
+    public String name() {
+        return "Pepsi";
+    }
+
+    /**
+     * 给百事定价
+     */
+    @Override
+    public float price() {
+        return 35.0f;
+    }
+}

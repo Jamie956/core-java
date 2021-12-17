6a2d10727bf42ac80b61b4b8b9b71daf3ceef0e5
Jamie956 <752492509@qq.com>
2021-12-18 00:09:14 +0800

    设计模式-装饰器模式

---
+++ basic/src/main/java/com/jamie/design/pattern/decorator/Rectangle.java
@@ -0,0 +1,24 @@
+package com.jamie.design.pattern.decorator;
+
+import lombok.Getter;
+
+/**
+ * 形状接口的实现类：正方形
+ */
+@Getter
+public class Rectangle implements Shape {
+    /**
+     * 构造
+     */
+    public Rectangle() {
+        System.out.println("正方形实例化");
+    }
+
+    /**
+     * 画画
+     */
+    @Override
+    public void draw() {
+        System.out.println("画正方形");
+    }
+}

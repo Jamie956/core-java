f5f551da70a33a82a5feef7784e1cb38fccfa0f0
Jamie956 <752492509@qq.com>
2022-01-20 17:42:08 +0800

    up

--- basic/src/main/java/com/jamie/concurrency/lock/ConditionTest.java
+++ basic/src/main/java/com/jamie/concurrency/lock/ConditionTest.java
@@ -8,7 +8,7 @@ public class ConditionTest {
     ReentrantLock lock = new ReentrantLock();
     Condition condition = lock.newCondition();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void case1() throws InterruptedException {
         ConditionTest conditionTest = new ConditionTest();
         Condition condition = conditionTest.condition;
         ReentrantLock lock = conditionTest.lock;
@@ -19,6 +19,7 @@ public class ConditionTest {
                 System.out.println(Thread.currentThread().getName() + " before lock condition await");
                 //断点，suspend 设为 Thread
                 condition.await();
+                //断点，suspend 设为 Thread
                 System.out.println(Thread.currentThread().getName() + " after lock condition await");
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -42,6 +43,60 @@ public class ConditionTest {
                 lock.unlock();
             }
         }).start();
+    }
+
+    public static void case2() throws InterruptedException {
+        ConditionTest conditionTest = new ConditionTest();
+        Condition condition = conditionTest.condition;
+        ReentrantLock lock = conditionTest.lock;
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " before lock condition await");
+                condition.await();
+                //断点
+                System.out.println(Thread.currentThread().getName() + " after lock condition await");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + " before lock condition await");
+                condition.await();
+                //断点
+                System.out.println(Thread.currentThread().getName() + " after lock condition await");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println(Thread.currentThread().getName() + " execute Signal");
+                //断点，上面两条线程此时都处于 await 状态
+                condition.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+    }
+    public static void main(String[] args) throws InterruptedException {
+//        case1();
+        case2();
     }
 
 }

e020a7c386cc2946f1b6b1af50129d129cabb489
Jamie956 <752492509@qq.com>
2022-03-10 09:33:15 +0800

    ReflectionFactory

--- basic/src/main/java/com/cat/pattern/factory/reflection/Client.java
+++ basic/src/main/java/com/cat/pattern/factory/reflection/Client.java
@@ -1,5 +1,9 @@
 package com.cat.pattern.factory.reflection;
 
+/**
+ * 简单工厂（反射）：缓存Class对象，根据反射构造实例
+ * 缺点：反射机制降低程序性能
+ */
 public class Client {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         ShapeReflectionFactory.register("rectangle", Rectangle.class);
--- basic/src/main/java/com/cat/pattern/factory/reflection/ShapeReflectionFactory.java
+++ basic/src/main/java/com/cat/pattern/factory/reflection/ShapeReflectionFactory.java
@@ -4,8 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 简单工厂（反射）：缓存对象class，根据class反射构造实例
- * 缺点：反射机制会降低程序的性能
+ * 简单工厂（反射）
  */
 public class ShapeReflectionFactory {
     /**
@@ -14,14 +13,14 @@ public class ShapeReflectionFactory {
     private static final Map<String, Class<?>> ACTIVITY_MAP = new HashMap<>();
 
     /**
-     * 注册 Class对象class
+     * 注册 Class对象
      */
     public static void register(String type, Class<?> productClazz) {
         ACTIVITY_MAP.put(type, productClazz);
     }
 
     /**
-     * 根据key，获取Class对象，反射构造Class的实例
+     * 根据注册key，反射构造Class的实例
      */
     public static Shape getObject(String type) throws IllegalAccessException, InstantiationException {
         Class<?> clazz = ACTIVITY_MAP.get(type);

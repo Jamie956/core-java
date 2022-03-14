1d683b13c5d116bd0dd666ad87c3ec2088dc682c
Jamie956 <752492509@qq.com>
2022-03-14 20:36:11 +0800

    gc

---
+++ basic/src/main/java/jvmlab/gc/FinalizeEscapeGC.java
@@ -0,0 +1,46 @@
+package jvmlab.gc;
+
+/**
+ * 测试 finalize 调用次数，对象的自救
+ * 结论：finalize 只会调一次，调用时可以挽救对象
+ */
+public class FinalizeEscapeGC {
+    public static FinalizeEscapeGC SAVE_HOOK = null;
+
+    public void isAlive() {
+        System.out.println("yes, i am still alive :)");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize method executed!");
+        //GC时重新指向实例
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        SAVE_HOOK = new FinalizeEscapeGC();
+
+        //先把对象设空 help GC
+        SAVE_HOOK = null;
+        System.gc();
+        //sleep 一会，等待调GC finalize
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no, i am dead :(");
+        }
+
+        SAVE_HOOK = null;
+        //GC 不会再调 finalize
+        System.gc();
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no, i am dead :(");
+        }
+    }
+}

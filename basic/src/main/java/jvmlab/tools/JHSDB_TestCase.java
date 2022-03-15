2a603180c327edcb40e331bc08e6f1a3065bc98d
Jamie956 <752492509@qq.com>
2022-03-15 15:47:58 +0800

    tools

---
+++ basic/src/main/java/jvmlab/tools/JHSDB_TestCase.java
@@ -0,0 +1,29 @@
+package jvmlab.tools;
+
+
+/**
+ * staticObj、 instanceObj、 localObj存放在哪里？
+ * VM 参数：-Xmx10m -XX:+UseSerialGC -XX:-UseCompressedOops
+ *
+ * cd /path/to/jdk/lib
+ * java -cp sa-jdi.jar sun.jvm.hotspot.HSDB
+ */
+public class JHSDB_TestCase {
+    static class Test {
+        static ObjectHolder staticObj = new ObjectHolder();
+        ObjectHolder instanceObj = new ObjectHolder();
+
+        void foo() {
+            ObjectHolder localObj = new ObjectHolder();
+            System.out.println("done"); // 这里设一个断点
+        }
+    }
+
+    private static class ObjectHolder {
+    }
+
+    public static void main(String[] args) {
+        Test test = new JHSDB_TestCase.Test();
+        test.foo();
+    }
+}
---
+++ basic/src/main/java/jvmlab/tools/JstackTest.java
@@ -0,0 +1,15 @@
+package jvmlab.tools;
+
+/**
+ * 堆栈跟踪工具：jstack [ option ] vmid
+ * jstack -l 9628
+ */
+public class JstackTest {
+    public static void main(String[] args) throws InterruptedException {
+        int _1MB = 1024 * 1024;
+        while (true) {
+            Thread.sleep(500);
+            byte[] bigSize = new byte[2 * _1MB];
+        }
+    }
+}

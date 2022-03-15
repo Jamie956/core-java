e776b1a41e60e32392f572aca63db269c69ae4c3
Jamie956 <752492509@qq.com>
2022-03-15 16:09:27 +0800

    tools

---
+++ basic/src/main/java/jvmlab/tools/JconsoleTest.java
@@ -0,0 +1,28 @@
+package jvmlab.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * -Xms100m -Xmx100m -XX:+UseSerialGC
+ */
+public class JconsoleTest {
+    static class OOMObject {
+        public byte[] placeholder = new byte[64 * 1024];
+    }
+
+    public static void fillHeap(int num) throws InterruptedException {
+        Thread.sleep(5000);
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        for (int i = 0; i < num; i++) {
+            // 稍作延时， 令监视曲线的变化更加明显
+            Thread.sleep(50);
+            list.add(new OOMObject());
+        }
+        System.gc();
+    }
+
+    public static void main(String[] args) throws Exception {
+        fillHeap(1000);
+    }
+}

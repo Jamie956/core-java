6f77f8d2061b09b67813a914d0d530e2685f747e
Jamie956 <752492509@qq.com>
2022-03-15 15:16:57 +0800

    tools

---
+++ basic/src/main/java/jvmlab/tools/JinfoTest.java
@@ -0,0 +1,14 @@
+package jvmlab.tools;
+
+/**
+ * 查看配置信息：jinfo [option] pid
+ * VM 参数：-Xms20m -Xmx20m
+ * jinfo -flag CMSInitiatingOccupancyFraction 8028
+ */
+public class JinfoTest {
+    public static void main(String[] args) {
+        while (true) {
+
+        }
+    }
+}
---
+++ basic/src/main/java/jvmlab/tools/JmapTest.java
@@ -0,0 +1,16 @@
+package jvmlab.tools;
+
+/**
+ * 内存映像工具：jmap [ option ] vmid
+ * jmap -dump:format=b,file=eclipse.bin 8888
+ * 导出堆快照后，使用 jhat 命令分析：jhat eclipse.bin
+ */
+public class JmapTest {
+    public static void main(String[] args) throws InterruptedException {
+        int _1MB = 1024 * 1024;
+        while (true) {
+            Thread.sleep(500);
+            byte[] bigSize = new byte[2 * _1MB];
+        }
+    }
+}

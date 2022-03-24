83420a60adad062e7234a95437d927d23c90d442
Jamie956 <752492509@qq.com>
2022-03-24 23:32:09 +0800

    mv maven

---
+++ maven/assembly-component-descriptors/src/main/java/org/cat/App.java
@@ -0,0 +1,7 @@
+package org.cat;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
---
+++ maven/assembly-jar-with-dep/src/main/java/com/cat/Application.java
@@ -0,0 +1,10 @@
+package com.cat;
+
+import com.google.gson.Gson;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("参数："+args);
+        new Gson();
+    }
+}

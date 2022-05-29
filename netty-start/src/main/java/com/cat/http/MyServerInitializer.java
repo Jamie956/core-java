f0addaa3d914fdc8d9f10ab21b417bb3479560e4
Jamie956 <752492509@qq.com>
2022-05-29 22:27:48 +0800

    http

---
+++ netty-start/src/main/java/com/cat/http/MyServerInitializer.java
@@ -0,0 +1,17 @@
+package com.cat.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        //http 协议
+        pipeline.addLast("MyHttpServerCodec", new HttpServerCodec());
+        pipeline.addLast("MyHttpServerHandler", new MyHttpServerHandler());
+
+    }
+}

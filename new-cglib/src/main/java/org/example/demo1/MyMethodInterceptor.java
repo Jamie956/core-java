ad9363a92f76a58b31e1a956c4e76da4d3809bd8
Jamie956 <752492509@qq.com>
2022-11-02 20:20:28 +0800

    up

---
+++ new-cglib/src/main/java/org/example/demo1/MyMethodInterceptor.java
@@ -0,0 +1,16 @@
+package org.example.demo1;
+
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class MyMethodInterceptor implements MethodInterceptor {
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        System.out.println("before");
+        Object object = proxy.invokeSuper(obj, args);
+        System.out.println("after");
+        return object;
+    }
+}
---
+++ new-cglib/src/main/java/org/example/demo1/StartTest.java
@@ -0,0 +1,74 @@
+package org.example.demo1;
+
+import net.sf.cglib.beans.BeanGenerator;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.FixedValue;
+import net.sf.cglib.proxy.MethodInterceptor;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
+public class StartTest{
+    @Test
+    public void fixedValueTest() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(PersonService.class);
+        enhancer.setCallback((FixedValue) () -> "Hello Tom!");
+
+        PersonService proxy = (PersonService) enhancer.create();
+        //didn't invoke proxy method, only return fixed value
+        String res = proxy.sayHello(null);
+
+        assertEquals("Hello Tom!", res);
+    }
+
+    @Test
+    public void interceptorTest() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(PersonService.class);
+        enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+            if (method.getDeclaringClass() != Object.class && method.getReturnType() == String.class) {
+                //will not invoke target method, only return value
+                return "Hello Tom!";
+            } else {
+                //invoke target method
+                return proxy.invokeSuper(obj, args);
+            }
+        });
+        PersonService proxy = (PersonService) enhancer.create();
+
+        String hello = proxy.sayHello(null);
+        assertEquals("Hello Tom!", hello);
+
+        int lengthOfName = proxy.lengthOfName("Mary");
+        assertEquals(4, lengthOfName);
+    }
+
+    @Test
+    public void interceptorTest2() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(PersonService.class);
+        enhancer.setCallback(new MyMethodInterceptor());
+        PersonService proxy = (PersonService) enhancer.create();
+
+        String hello = proxy.sayHello("Tom");
+        assertEquals("Hello Tom", hello);
+    }
+
+    @Test
+    public void beanCreatorTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        BeanGenerator beanGenerator = new BeanGenerator();
+
+        beanGenerator.addProperty("name", String.class);
+        Object myBean = beanGenerator.create();
+
+        Method setter = myBean.getClass().getMethod("setName", String.class);
+        setter.invoke(myBean, "some string value set by a cglib");
+
+        Method getter = myBean.getClass().getMethod("getName");
+        assertEquals("some string value set by a cglib", getter.invoke(myBean));
+    }
+}

2a2b2767c4f9907d5ce8690caf7a86d937625d52
Jamie956 <752492509@qq.com>
2023-06-09 22:31:06 +0800

    Method annotaion

--- object-oriented/src/main/java/com/example/annotation/AnnotationTest.java
+++ object-oriented/src/main/java/com/example/annotation/AnnotationTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class AnnotationTest {
     @Test
@@ -19,5 +20,12 @@ public class AnnotationTest {
         Field valField = UsingFieldAnnotation.class.getDeclaredField("val");
         String name = valField.getAnnotation(FieldLevelAnnotation.class).name();
         Assert.assertEquals("default value", name);
+    }
+
+    @Test
+    public void methodLevelAnnotationTest() throws NoSuchMethodException {
+        Method methodHalo = UsingMethodAnnotation.class.getMethod("halo");
+        String name = methodHalo.getAnnotation(MethodLevelAnnotation.class).name();
+        Assert.assertEquals("halo", name);
     }
 }

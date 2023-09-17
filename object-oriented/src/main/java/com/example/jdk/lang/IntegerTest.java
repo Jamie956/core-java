25c4187e8a244c0f53a87db95f282a681a633a3c
Jamie956 <752492509@qq.com>
2023-09-17 23:03:24 +0800

    jdk integer method test and explanation

--- object-oriented/src/main/java/com/example/jdk/lang/IntegerTest.java
+++ object-oriented/src/main/java/com/example/jdk/lang/IntegerTest.java
@@ -43,6 +43,7 @@ public class IntegerTest {
         // Integer to unsigned number string in the 16 radix
         Assert.assertEquals("f", Integer.toHexString(15));
         Assert.assertEquals("fffffff1", Integer.toHexString(-15));
+
         Assert.assertEquals(Integer.toUnsignedString(-15, 16), Integer.toHexString(-15));
     }
 
@@ -63,72 +64,68 @@ public class IntegerTest {
 
     @Test
     public void toBinaryString() {
-        String s = Integer.toBinaryString(15);
+        // Integer to unsigned number string in the 2 radix
+        Assert.assertEquals("1", Integer.toBinaryString(1));
+        Assert.assertEquals("10", Integer.toBinaryString(2));
+        Assert.assertEquals("1111", Integer.toBinaryString(15));
+
+        // 1111 1111 1111 1111 1111 1111 1111 1111
+        Assert.assertEquals("11111111111111111111111111111111", Integer.toBinaryString(-1));
+        Assert.assertEquals("11111111111111111111111111111110", Integer.toBinaryString(-2));
+        Assert.assertEquals("11111111111111111111111111110001", Integer.toBinaryString(-15));
     }
 
-
-
     @Test
-    public void toUnsignedString1() {
-        String s = Integer.toUnsignedString(15);
-        String a = Integer.toUnsignedString(-15);
+    public void toUnsignedString10() {
+        // Integer to unsigned number string in the 10 radix
+        Assert.assertEquals("1", Integer.toUnsignedString(1));
+        Assert.assertEquals("15", Integer.toUnsignedString(15));
+
+        Assert.assertEquals(String.valueOf(16*16*16*16*16*16*16*16L - 1), Integer.toUnsignedString(-1));
+        // 1111 1111 1111 1111 1111 1111 1111 0001
+        Assert.assertEquals(String.valueOf(16*16*16*16*16*16*16*16L - 15), Integer.toUnsignedString(-15));
     }
 
     @Test
     public void parseInt() {
-        int i = Integer.parseInt("123", 10);
-        int j = Integer.parseInt("-123", 10);
-        int a = Integer.parseInt("1111", 2);
+        // the specific radix string sign number to 10 radix Integer number
+        Assert.assertEquals(1, Integer.parseInt("1", 2));
+        Assert.assertEquals(-1, Integer.parseInt("-1", 2));
+        Assert.assertEquals(15, Integer.parseInt("1111", 2));
+        Assert.assertEquals(-15, Integer.parseInt("-1111", 2));
+
+        Assert.assertEquals(1, Integer.parseInt("1", 10));
+        Assert.assertEquals(-1, Integer.parseInt("-1", 10));
     }
 
     @Test
     public void parseUnsignedInt() {
-        int i = Integer.parseUnsignedInt("123", 10);
-        int a = Integer.parseUnsignedInt("1111", 2);
+        // the specific radix string unsigned number to 10 radix Integer number
+        Assert.assertEquals(1, Integer.parseUnsignedInt("1", 10));
+        Assert.assertEquals(15, Integer.parseUnsignedInt("15", 10));
+
+        Assert.assertEquals(1, Integer.parseUnsignedInt("1", 2));
+        Assert.assertEquals(-1, Integer.parseUnsignedInt("11111111111111111111111111111111", 2));
+        Assert.assertEquals(15, Integer.parseUnsignedInt("1111", 2));
+        Assert.assertEquals(-15, Integer.parseUnsignedInt("11111111111111111111111111110001", 2));
     }
 
     @Test
     public void valueOf() {
-        Integer i = Integer.valueOf("1111", 2);
-    }
+        // the specific radix sign number string to Integer in 10 radix
+        Assert.assertEquals(new Integer(15), Integer.valueOf("15", 10));
+        Assert.assertEquals(new Integer(-15), Integer.valueOf("-15", 10));
 
-    @Test
-    public void valueOf1() {
-        Integer i = Integer.valueOf("1111");
-    }
-
-    @Test
-    public void valueOf2() {
-        Integer i = Integer.valueOf(122);
-        Integer j = Integer.valueOf(128);
-    }
-
-    @Test
-    public void Integer() {
-        Integer i = new Integer(5);
-    }
-
-    @Test
-    public void Integer1() {
-        Integer i = new Integer("67");
-    }
-
-    @Test
-    public void hashCode1() {
-        Integer i = new Integer("67");
-        int c = i.hashCode();
-    }
-
-    @Test
-    public void getInteger() {
-        Integer i = Integer.getInteger("sun.arch.data.model");
+        Assert.assertEquals(new Integer(15), Integer.valueOf("1111", 2));
+        Assert.assertEquals(new Integer(-15), Integer.valueOf("-1111", 2));
     }
 
     @Test
     public void compareTo() {
-        Integer a = new Integer(67);
-        Integer b = new Integer(1);
-        int r = a.compareTo(b);
+        Integer a = 1;
+        Integer b = 2;
+        Assert.assertEquals(-1, a.compareTo(b));
+        Assert.assertEquals(1, b.compareTo(a));
     }
 
     @Test

70ff6161cedf26a6bd2b416d6dc5caf674b5434b
Jamie956 <752492509@qq.com>
2023-09-29 20:00:36 +0800

    up

--- object-oriented/src/main/java/com/io/BlokingIOTest.java
+++ object-oriented/src/main/java/com/io/BlokingIOTest.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class BlokingIOTest {
     /**
@@ -17,33 +15,24 @@ public class BlokingIOTest {
     @Test
     public void server() throws IOException {
         ServerSocket serverSocket = new ServerSocket(6666);
-        ExecutorService pool = Executors.newFixedThreadPool(10);
 
-        while (true) {
-            System.out.println("等待下一个客户端连接...");
-            //accept 阻塞监听，等待客户端连接
-            Socket socket = serverSocket.accept();
-            System.out.println("连接成功!");
+        System.out.println("等待下一个客户端连接...");
+        //accept 阻塞监听，等待客户端连接
+        Socket socket = serverSocket.accept();
+        System.out.println("连接成功!");
 
-            pool.execute(() -> {
-                System.out.println("分配线程与新连接通信 " + Thread.currentThread().getName());
-
-                //获取socket 输入流
-                try (InputStream in = socket.getInputStream()) {
-                    byte[] bytes = new byte[1024];
-                    while (true) {
-                        int length = in.read(bytes);
-                        if (length != -1) {
-                            System.out.println(new String(bytes, 0, length));
-                        } else {
-                            break;
-                        }
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+        try (InputStream in = socket.getInputStream()) {
+            byte[] bytes = new byte[1024];
+            while (true) {
+                int length = in.read(bytes);
+                if (length != -1) {
+                    System.out.println(new String(bytes, 0, length));
+                } else {
+                    break;
                 }
-            });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 

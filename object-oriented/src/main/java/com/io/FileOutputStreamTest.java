db74eca28b4327962f11967be7489521950c5804
Jamie956 <752492509@qq.com>
2023-10-10 07:50:51 +0800

    up

--- object-oriented/src/main/java/com/io/FileOutputStreamTest.java
+++ object-oriented/src/main/java/com/io/FileOutputStreamTest.java
@@ -1,5 +1,6 @@
 package com.io;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
@@ -8,23 +9,25 @@ import java.nio.channels.FileChannel;
 public class FileOutputStreamTest {
     @Test
     public void cons() throws FileNotFoundException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
     }
 
     @Test
     public void cons2() throws FileNotFoundException {
-        FileOutputStream fo = new FileOutputStream("resources/source", true);
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source", true);
     }
 
     @Test
     public void cons3() throws FileNotFoundException {
-        FileOutputStream fo = new FileOutputStream(new File("resources/source"));
+        FileOutputStream fo = new FileOutputStream(new File("src/main/resources/source"));
     }
 
     @Test
     public void cons4() throws IOException {
-        FileOutputStream fo = new FileOutputStream(new File("resources/source"), true);
+        FileOutputStream fo = new FileOutputStream(new File("src/main/resources/source"), true);
         fo.write(97);
+
+        Assert.assertEquals(97, new FileInputStream("src/main/resources/source").read());
     }
 
     @Test
@@ -34,39 +37,49 @@ public class FileOutputStreamTest {
 
     @Test
     public void write() throws IOException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
         fo.write(97);
+
+        Assert.assertEquals(97, new FileInputStream("src/main/resources/source").read());
     }
 
     @Test
     public void writeBytes() throws IOException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
         byte[] bytes = new byte[]{97, 98};
         fo.write(bytes);
+
+        FileInputStream in = new FileInputStream("src/main/resources/source");
+        Assert.assertEquals(97, in.read());
+        Assert.assertEquals(98, in.read());
     }
 
     @Test
     public void writeBytesOff() throws IOException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
         byte[] bytes = new byte[]{97, 98, 98, 98};
         fo.write(bytes, 1, 2);
+
+        FileInputStream in = new FileInputStream("src/main/resources/source");
+        Assert.assertEquals(98, in.read());
+        Assert.assertEquals(98, in.read());
     }
 
     @Test
     public void close() throws IOException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
         fo.close();
     }
 
     @Test
     public void getFD() throws IOException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
         FileDescriptor fd = fo.getFD();
     }
 
     @Test
     public void getChannel() throws IOException {
-        FileOutputStream fo = new FileOutputStream("resources/source");
+        FileOutputStream fo = new FileOutputStream("src/main/resources/source");
         FileChannel channel = fo.getChannel();
     }
 }
